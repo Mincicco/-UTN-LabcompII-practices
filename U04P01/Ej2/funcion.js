165de10fa645ec7eb499854a35f291d9122ba5c2
Mincicco <manuciriaco@gmail.com>
2022-10-04 08:26:29 -0300

    ejercicios javacript

---
+++ U04P01/Ej2/funcion.js
@@ -0,0 +1,10 @@
+/*Cargar dos números en objetos de tipo text y al presionar un
+botón, mostrar el mayor.*/ 
+
+var boto = document.getElementById("boton");
+
+function potencia(){
+    var num = document.getElementById("flama");
+    num1 = num.value
+    alert(num1 * num1)
+}
